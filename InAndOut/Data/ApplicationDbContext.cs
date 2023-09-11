2377093616cc14a5cb86a2698b80f9d219792543
Akhilesh <360akhileshsingh.it@gmail.com>
2023-09-11 23:14:29 +0530

    Create a model class expence.

--- InAndOut/Data/ApplicationDbContext.cs
+++ InAndOut/Data/ApplicationDbContext.cs
@@ -20,7 +20,6 @@ namespace InAndOut.Data
                 new Item { Id = 2, Borrower = "Harish", ItemName="Truck", Lender="Rajesh"},   
                 new Item { Id = 3, Borrower = "Nitesh", ItemName = "Car", Lender = "Devanti" },
                 new Item { Id = 5, Borrower = "Dhillon", ItemName = "Bike", Lender = "Hero" }
-
                 );
         }         
     }
