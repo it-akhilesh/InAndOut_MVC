415d554c215d59979f4872c116f291c7afaf792b
Akhilesh <360akhileshsingh.it@gmail.com>
2023-09-13 23:53:39 +0530

    Delete Entry in the Database.

--- InAndOut/Controllers/ExpenseController.cs
+++ InAndOut/Controllers/ExpenseController.cs
@@ -37,6 +37,39 @@ namespace InAndOut.Controllers
             }
             return View(obj);
         }
+        // get delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int? id)
+        {
+            
+            if (id == null || id==0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Expenses.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        // post Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Expenses.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Expenses.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }  
 
     }
 }
